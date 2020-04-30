4e2d5dcd0e5e7c7122184264f1756b91f9fbd349
seedubya <chrisjwood@outlook.com>
2020-04-30 19:57:42 +0100

    added UID, GID to correct file creation issues.

--- docker_run_samba.sh
+++ docker_run_samba.sh
@@ -6,15 +6,16 @@
 docker run --detach \
     -e GLOBAL="ntlm auth=yes" \
     -e GLOBAL="server role = standalone server" \
+    -e GROUPID="65534" \
     -e NMBD="yes" \
     -e SHARE="music library;/share/music;yes;no;yes;all;none;;Music Library on Zebedee" \
     -e SMB="1" \
     -e TZ="Europe/London" \
+    -e USERID="65534" \
     -e WORKGROUP="WORKGROUP" \
     -p 139:139 \
     -p 445:445 \
     -v /share/music:/share/music \
     --name samba \
-    --net=host \
     --restart=unless-stopped \
     dperson/samba:latest
