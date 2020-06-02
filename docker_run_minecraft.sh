2946ff44d785a7c03691c582a7a9098558d86aae
seedubya <chrisjwood@outlook.com>
2020-06-02 07:47:09 +0100

    new file.

---
+++ docker_run_minecraft.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+#
+# Minecraft server - bedrock
+#
+
+docker run --detach \
+    -e AUTOPAUSE_TIMEOUT_EST="900" \
+    -e AUTOPAUSE_TIMEOUT_INIT="300" \
+    -e ENABLE_AUTOPAUSE="TRUE" \
+    -e EULA="TRUE" \
+    -e TZ="Europe/London" \
+    -p 25565:25565 \
+    -v "/home/pi/docker_config/minecraft:/data" \
+    --name minecraft \
+    --restart=unless-stopped \
+    itzg/minecraft:latest
+
