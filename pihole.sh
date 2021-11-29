0205f81af5431fab1db288620e1179813a2d2dc4
seedubya <chrisjwood@outlook.com>
2021-11-29 07:36:27 +0000

    updates based on new docker instructions.

--- pihole.sh
+++ pihole.sh
@@ -10,17 +10,18 @@
 #
 
 docker run --detach \
-    -e DNS1="208.67.222.222" \
-    -e DNS2="208.67.220.220" \
-    -e ServerIP="192.168.1.110" \
+    -e PIHOLE_DNS_1="208.67.222.222" \
+    -e PIHOLE_DNS_2="208.67.220.220" \
+    -e FTLCONF_REPLY_ADDR4="192.168.1.10" \
     -e TZ="Europe/London" \
-    -e WEBPASSWORD="yourehavingagiraffe" \
+    -e WEBPASSWORD="ChangeMe" \
     -p 53:53/tcp \
     -p 53:53/udp \
     -p 80:80 \
     -p 443:443 \
     -v "/home/pi/docker_config/pihole/etc-pihole/:/etc/pihole/" \
     -v "/home/pi/docker_config/pihole/etc-dnsmasq.d/:/etc/dnsmasq.d/" \
+    --cap-add=NET_ADMIN \
     --dns=127.0.0.1 --dns=1.1.1.1 \
     --name pihole \
     --restart=unless-stopped \
