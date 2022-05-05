015c15210d4acae53b21359c45b59915812439bb
seedubya <chrisjwood@outlook.com>
2022-05-05 10:55:43 +0100

    updates to plex server details, still not working.

--- plex.sh
+++ plex.sh
@@ -3,16 +3,16 @@
 # plex Server in docker.
 #
 # Update procedure (TBC):
-# 1. docker pull ghcr.io/linuxserver/plex:arm64v8-latest
+# 1. docker pull lscr.io/linuxserver/plex:latest
 # 2. docker stop plex ; docker container rm plex
 # 3. ./plex.sh
 # 4.  remove old image of plex: (docker image ls ; docker image rm <image id>)
 #
 
 docker run --detach \
-    -e GID=1000 \
-    -e UID=1000 \
     -e PLEX_CLAIM="claim-ws7K2XP7-zXtZGL4Szcr" \
+    -e PGID=1000 \
+    -e PUID=1000 \
     -e TZ="Europe/London" \
     -v /home/pi/docker_config/plex/:/config \
     -v /mnt/mydisk/Transcode:/transcode \
@@ -22,4 +22,4 @@ docker run --detach \
     --net=host \
     --privileged \
     --restart=unless-stopped \
-    ghcr.io/linuxserver/plex:arm64v8-latest
+    lscr.io/linuxserver/plex:latest
